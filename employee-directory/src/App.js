96648aa3b80acdf1ac4638b0308d809a63781388
Denzell Grant <denzgrant@gmail.com>
2020-07-12 21:41:07 -0700

    added skeleton and banner

---
+++ employee-directory/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./components/header"
+import Buttons from "./components/buttons"
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Buttons />
+    </>
+  );
+}
+
+export default App;
---
+++ employee-directory/src/components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Heading = styled.header`
+  font-size: 5rem;
+  border: 5px solid black;
+  padding: 2em;
+  background: yellow;
+  margin: 0; 
+  background-image: url(${'/xmen_logo.png'});
+  background-repeat: no-repeat;
+  background-position: center;
+
+`
+
+const Header = () => {
+  return (
+    <Heading imgUrl={process.env.PUBLIC_URL + 'xmen_logo.png'}></Heading>
+
+  );
+}
+
+export default Header;
+
