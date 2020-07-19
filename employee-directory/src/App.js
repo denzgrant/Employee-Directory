cc7065b607f2599d6c1f183ec263a3c1056b4691
Denzell Grant <denzgrant@gmail.com>
2020-07-18 20:26:58 -0700

    deployed to heroku

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import Header from "./components/header";
 import MaterialEmployTable from "./components/table"
-import employees from "./employees.json";
+
 
 
 
 class App extends Component {
-  state = {
-    employees
-  };
 
-  
   render() {
     return (
       <>
